00d75c72d1f53d218d462eec0b477702890b1cf2
AmritPal Singh <iamritpalsinghh@gmail.com>
2024-05-28 20:32:08 +0530

    connected the logout page with the backend and its working fine.

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,12 +1,19 @@
-
+import { useContext, useEffect } from "react";
+import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
 
 
 
 
 const Logout = () => {
 
+    const { setCurrentUser } = useContext(UserContext);
+    const navigate = useNavigate();
+    setCurrentUser(null);
+    navigate("/login");
+
     return (
-        <div><h1>Logout</h1></div>
+        <></>
     )
 }
 
